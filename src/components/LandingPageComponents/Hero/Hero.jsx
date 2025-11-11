0a4b5ef9966c54cef645fdd8f56c256b361dff89
neoDaren <pirmatovzhox@gmail.com>
2025-11-11 13:11:15 +0500

    Main Page

--- src/components/LandingPageComponents/Hero/Hero.jsx
+++ src/components/LandingPageComponents/Hero/Hero.jsx
@@ -1,8 +1,29 @@
 import './style.scss'
-
+import logo from './images/taskify_logo.png'
+// import Dashboard from './images/Dashboard.png'
 function Hero() {
-    return <div className="HeroLandingPage">
-        Hero LandingPage
+    return <div>
+    <header className='navbar_header'>
+            <img src={logo} alt="Taskify_logo" className='logo'/>
+            <div className='navbar'>
+                <a href="/" className='nav'>Home</a>
+                <a href="/" className='nav'>Solutions</a>
+                <a href="/" className='nav'>Contact</a>
+            </div>
+            <button className='Start'> start now </button>
+        </header> <div className="HeroLandingPage">
+        
+
+      <section className='Hero_start'>
+            <div className='reviews'></div>
+            <h1 className='main_title'>Every big goal starts with a small task.</h1>
+            <p className='stay_focused'>Stay focused, stay organized, and make every day count with your new task manager.</p>
+            <button className='Hero_button'>Get startesd</button>
+        </section>
+
+            {/* <img src={Dashboard} alt="" className='Dashboard'/> */}
+
+    </div>
     </div>
 }
 export default Hero;
