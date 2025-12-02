0d757b7969856266da91890e6db252920f2af603
Diyor Adashev <adashevdiyor443@gmail.com>
2025-12-02 15:54:28 +0500

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import SolutionsPage from "./pages/SolutionsPage";
 import AboutPage from "./pages/AboutPage";
 import LandingPage from "./pages/LandingPage";
+import TeamsPage from "./pages/TeamsPage";
+import TeamPage from "./pages/TeamPage";
 import Navbar from "./components/Navbar/Navbar";
+import DashboardLayout from "./components/Layout/DashboardLayout";
 
 function App() {
-
-    return (<div className={'container-fluid'}>
-
+    return (
+        <div className={'container-fluid'}>
             <Routes>
-                <Route path={'/'} element={<Navbar/>}>
-                    <Route path="/" element={<LandingPage/>}/>
-                    <Route path="/solutions" element={<SolutionsPage/>}/>
-                    <Route path="/about" element={<AboutPage/>}/>
-                </Route> 
+                {/* Public routes with Navbar */}
+                <Route path={'/'} element={<Navbar />}>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/solutions" element={<SolutionsPage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                </Route>
+
+                {/* Dashboard routes with Sidebar */}
+                <Route element={<DashboardLayout />}>
+                    <Route path="/teams" element={<TeamsPage />} />
+                    <Route path="/team/:teamId" element={<TeamPage />} />
+                    <Route path="/my-tasks" element={<TeamsPage />} />
+                </Route>
             </Routes>
         </div>
-    )
-        ;
+    );
 }
-
 
 export default App;
---
+++ src/components/Layout/DashboardLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Sidebar from '../Sidebar/Sidebar';
+import './DashboardLayout.scss';
+
+const DashboardLayout = () => {
+    return (
+        <div className="dashboard-layout">
+            <Sidebar />
+            <main className="main-content">
+                <Outlet />
+            </main>
+        </div>
+    );
+};
+
+export default DashboardLayout;
